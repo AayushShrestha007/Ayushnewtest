4e50fb27090c241b4800d8ac0336ebee90c2bf67
AayushShrestha007 <ayooshshrestha@gmail.com>
2022-07-13 10:27:12 +0545

    new

--- hello.py
+++ hello.py
@@ -1,16 +1,15 @@
 print("abc")
 print("her there")
 #changes made in new branches
-=======
+
 
 #changes in main branch 
 
-<<<<<<< HEAD
-=======
+
 #changes made in new branches
 
 
 #changes in main branch 
 
 #new changes in main branch
->>>>>>> main
+
